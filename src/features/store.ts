884fb656798ee2eef039fa4396dc4ea6e1dfc169
Ferryal Fajar <ferryal21@gmail.com>
2025-12-06 18:08:33 +0700

    feat: setup redux, store pokemon & species

---
+++ src/features/store.ts
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { cachedPokemonsReducer } from "./cachedPokemonsSlice";
+import { evolutionChainReducer } from "./evolutionChainSlice";
+import { pokemonsReducer } from "./pokemonSlice";
+import { speciesReducer } from "./speciesSlice";
+
+export const rootReducer = combineReducers({
+  cachedPokemons: cachedPokemonsReducer,
+  pokemons: pokemonsReducer,
+  species: speciesReducer,
+  evolutionChain: evolutionChainReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
